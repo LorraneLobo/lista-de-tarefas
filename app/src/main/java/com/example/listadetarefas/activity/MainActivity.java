05e1b38ad452fd86159f925d24d2f6a73f2e2892
Lorrane Alves <Lorrane.dev@gmail.com>
2021-11-11 18:36:52 -0300

    Adicionado menu e Banco de Dados configurado

--- app/src/main/java/com/example/listadetarefas/activity/MainActivity.java
+++ app/src/main/java/com/example/listadetarefas/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.listadetarefas.activity;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -16,6 +17,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.listadetarefas.adapter.TarefaAdapter;
 import com.example.listadetarefas.databinding.ActivityMainBinding;
+import com.example.listadetarefas.helper.DbHelper;
 import com.example.listadetarefas.helper.RecyclerItemClickListener;
 import com.example.listadetarefas.model.Tarefa;
 
@@ -96,6 +98,14 @@ public class MainActivity extends AppCompatActivity {
         //Configurar um adapter
         tarefaAdapter = new TarefaAdapter(listaTarefas);
 
+        DbHelper db = new DbHelper(getApplicationContext());
+
+        ContentValues cv = new ContentValues();
+        cv.put("nome", "Teste");
+
+        db.getWritableDatabase().insert("tarefas", null, cv);
+
+
         //Configurar Recyclerview
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerListaTarefas.setLayoutManager(layoutManager);
@@ -112,20 +122,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_adicionar_tarefa, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.itemSalvar) {
             return true;
         }
 
